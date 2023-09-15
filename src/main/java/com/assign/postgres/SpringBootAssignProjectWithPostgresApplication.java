4c84460585a9361f47ac89c6db2ef131697ec2e5
mamish12 <mku20sahu@gmail.com>
2023-09-15 12:00:59 +0530

    start

---
+++ src/main/java/com/assign/postgres/SpringBootAssignProjectWithPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.assign.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAssignProjectWithPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssignProjectWithPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assign/postgres/SpringBootAssignProjectWithPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assign.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssignProjectWithPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
